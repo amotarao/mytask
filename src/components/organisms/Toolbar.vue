7716275503ffe5804ad05b641302c2df6de8b6f5
Amon Sawamura <amon.sawamura@gmail.com>
2018-12-16 15:52:12 +0900

    feature: avatar

--- src/components/organisms/Toolbar.vue
+++ src/components/organisms/Toolbar.vue
@@ -3,7 +3,38 @@
     <v-toolbar-title class="white--text">My Task</v-toolbar-title>
     <v-spacer />
 
-    <v-btn flat>Sign in</v-btn>
-    <v-btn outline color="white">Sign up</v-btn>
+    <template v-if="!isSignedIn">
+      <v-btn @click="startSignIn" flat>Sign in</v-btn>
+      <v-btn @click="startSignIn" outline color="white">Sign up</v-btn>
+    </template>
+    <template v-else>
+      <v-avatar size="36px">
+        <img :src="user.photoURL" alt="Avatar" />
+      </v-avatar>
+    </template>
   </v-toolbar>
 </template>
+
+<script>
+import { signIn } from '@/plugins/firebase'
+
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'toolbar',
+  computed: {
+    ...mapGetters('user', ['user', 'isSignedIn']),
+  },
+  methods: {
+    startSignIn() {
+      signIn()
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.user-icon {
+  overflow: hidden;
+}
+</style>

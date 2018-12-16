09e3d2a8a6dc78e61e75903a3797e511d88cc3d6
Amon Sawamura <amon.sawamura@gmail.com>
2018-12-16 10:37:10 +0900

    feature: edit button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,40 @@
 <template>
-  <div class="home">
-    <v-container grid-list-md>
-      <v-layout row wrap>
-        <v-flex xs6> <task-list title="今日のタスク" /> </v-flex>
-        <v-flex xs6> <task-list title="いつかやるタスク" /> </v-flex>
-      </v-layout>
-    </v-container>
-  </div>
+  <v-container grid-list-sm>
+    <v-layout row wrap>
+      <v-flex xs6> <task-list title="Today" :tasks="todayTasks" /> </v-flex>
+      <v-flex xs6> <task-list title="Future" :tasks="featureTasks" /> </v-flex>
+    </v-layout>
+    <v-dialog v-model="dialog" persistent max-width="800">
+      <v-btn fixed dark fab bottom right slot="activator" color="green">
+        <v-icon>add</v-icon>
+      </v-btn>
+      <task-edit @close="dialog = false" @save="addTask($event)" />
+    </v-dialog>
+  </v-container>
 </template>
 
 <script>
 import TaskList from '@/components/organisms/TaskList.vue'
+import TaskEdit from '@/components/organisms/TaskEdit.vue'
+
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
   name: 'home',
   components: {
     TaskList,
+    TaskEdit,
+  },
+  data() {
+    return {
+      dialog: false,
+    }
+  },
+  computed: {
+    ...mapGetters('tasks', ['allTasks', 'todayTasks', 'featureTasks']),
+  },
+  methods: {
+    ...mapActions('tasks', ['addTask', 'editTask', 'removeTask']),
   },
 }
 </script>
